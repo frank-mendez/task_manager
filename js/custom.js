95ffcba32526c84d8d5f4849dfebb9104b62949d
Frank Mendez <Frank Mendez>
2016-10-12 13:35:21 +0800

    Firebase test

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,36 @@
 /**
  * Created by frank on 12/10/2016.
  */
+
+//Module pattern
+var Module = {};
+
+Module.Firebase = (function(){
+
+    function init(){
+
+        var database = firebase.database();
+
+        database.ref('/task/').once('value').then(function(snapshot) {
+
+            var tasks = snapshot.val();
+
+            console.log(tasks);
+
+        });
+
+
+    }
+
+    return{
+        init: init
+    }
+
+})();
+
+
+$(document).ready(function(){
+
+    Module.Firebase.init();
+
+});
