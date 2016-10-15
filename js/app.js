63c471939ef9345e9d044c1cfa18ae8c6ca9e3f8
Frank Mendez <Frank Mendez>
2016-10-15 13:55:13 +0800

    Added Create Function Added Query by ID function Added Save selected Function

--- js/app.js
+++ js/app.js
@@ -23,23 +23,167 @@ function TaskListController($scope){
 
     var vm = this;
 
-    var database = firebase.database();
+    $scope.initializeData = function(){
 
-    database.ref('/task').once('value').then(function(snapshot) {
+        $scope.formTitle = 'Create Task';
+
+        $scope.data = {
+            status: 'to-do',
+            priority: 'low'
+        };
+
+        $scope.data.labels = [
+            {
+                text:''
+            }
+        ];
+
+        $scope.updateBtn = true;
+        $scope.saveBtn = false;
+
+    }
+
+    $scope.addLabel = function(){
+        $scope.data.labels.push({
+            text:''
+        });
+    };
+
+    $scope.firebase = function(){
+
+        var database = firebase.database();
+
+        database.ref('/task').once('value').then(function(snapshot) {
 
 
-        $scope.$apply(function(){
+            $scope.$apply(function(){
 
-            vm.items = snapshot.val();
+                vm.items = snapshot.val();
 
-            console.log(vm.items);
+            })
 
-        })
+        });
 
-    });
+    }
+
+    $scope.initializeData();
+    $scope.firebase();
 
 
+    $scope.submit = function(){
+
+        console.log($scope.data.labels);
+
+        var rootRef = firebase.database().ref();
+        var storesRef = rootRef.child('task');
+        var newStoreRef = storesRef.push();
+        newStoreRef.set({
+            description: $scope.data.description,
+            order: 3,
+            status: $scope.data.status,
+            priority: $scope.data.priority,
+            title: $scope.data.title
+        });
+
+        var newID = newStoreRef.getKey();
+
+        var label = rootRef.child('task/' + newID + '/labels');
+
+        angular.forEach($scope.data.labels, function(value){
+
+            label.push(value.text);
+
+        });
+
+        var insertID = rootRef.child('task/' + newID + '/id' );
+        insertID.set(newID);
+
+        $scope.firebase();
+        $scope.initializeData();
+
+
+    }
+
+    $scope.cancel = function(){
+
+        $scope.initializeData();
+
+    }
+
+    $scope.update = function(taskID){
+
+        $scope.formTitle = 'Update Task';
+        $scope.updateBtn = false;
+        $scope.saveBtn = true;
+
+        console.log(taskID);
+
+        var root = firebase.database().ref();
+        root.child('task/' + taskID).once('value').then(function(snapshot){
+
+            queryData = snapshot.val();
+
+            //console.log(queryData.labels);
+
+            $scope.$apply(function(){
+
+                $scope.data = {
+                    description: queryData.description,
+                    id: queryData.id,
+                    order: queryData.order,
+                    status: queryData.status,
+                    priority: queryData.priority,
+                    title: queryData.title
+                };
+
+                $scope.data.labels = [];
+
+                angular.forEach(queryData.labels, function(value){
+
+                    $scope.data.labels.push({
+                        text: value
+                    });
+
+                });
+
+            });
+
+        });
+
+        $scope.save = function(){
+
+            //console.log($scope.data);
+
+            var root = firebase.database().ref();
+            var updateData = root.child('task/' + $scope.data.id);
+            updateData.set({
+
+                description: $scope.data.description,
+                order: 3,
+                id: $scope.data.id,
+                status: $scope.data.status,
+                priority: $scope.data.priority,
+                title: $scope.data.title
+
+            });
+
+            var updateLabels = root.child('task/' + $scope.data.id + '/labels');
+
+            angular.forEach($scope.data.labels, function(value){
+
+                updateLabels.push(value.text);
+
+            });
+
+            $scope.firebase();
+            $scope.initializeData();
+
+
+        }
+
+    }
 
 
 }
+
 
