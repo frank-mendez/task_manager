eed112113f0c003f1a3af0b42db815a8aa55579b
Frank Mendez <Frank Mendez>
2016-10-20 12:56:57 +0800

    added routes.js for route service

---
+++ js/routes.js
@@ -0,0 +1,24 @@
+/**
+ * Created by frank on 20/10/2016.
+ */
+
+(function() {
+    'use strict';
+
+    angular.module('taskManager')
+        .config(function($routeProvider) {
+
+            $routeProvider
+                .when('/',{
+                    templateUrl: 'task-view.html',
+                    controller: 'TaskListController as taskList'
+                })
+                .when('/task',{
+                    templateUrl: 'task.html',
+                    controller: 'TaskListController as taskList'
+                });
+
+        });
+
+
+})();
