811944776ec1f85493b7d213d2667c09a5fc83ee
Frank Mendez <Frank Mendez>
2016-10-22 12:38:35 +0800

    added new attribute for update and create functions

--- js/taskListController.js
+++ js/taskListController.js
@@ -24,6 +24,19 @@
 
         var vm = this;
 
+        $scope.firebaseInit = function(){
+
+            var config = {
+                apiKey: "AIzaSyC1mir7krdyGHx5jqE8yqWLLCL_4cAAQNg",
+                authDomain: "real-time-task-manager.firebaseapp.com",
+                databaseURL: "https://real-time-task-manager.firebaseio.com",
+                storageBucket: "real-time-task-manager.appspot.com",
+                messagingSenderId: "1058531001319"
+            };
+            firebase.initializeApp(config);
+
+        }
+
         $scope.initializeData = function(){
 
             $scope.formTitle = 'Create Task';
@@ -85,15 +98,17 @@
 
         }
 
-
         $scope.initializeData();
         $scope.firebase();
 
 
+        var date = new Date();
+        $scope.FromDate = date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
 
         $scope.submit = function(){
 
             //console.log($scope.data.labels);
+
 
             var counter = $scope.counter + 1;
 
@@ -105,7 +120,11 @@
                 order: counter,
                 status: $scope.data.status,
                 priority: $scope.data.priority,
-                title: $scope.data.title
+                title: $scope.data.title,
+                created_by: $scope.username,
+                modified_by: $scope.username,
+                date_added: $scope.FromDate,
+                date_modified: $scope.FromDate
             });
 
             var newID = newStoreRef.getKey();
@@ -160,7 +179,11 @@
                         order: queryData.order,
                         status: queryData.status,
                         priority: queryData.priority,
-                        title: queryData.title
+                        title: queryData.title,
+                        created_by: queryData.created_by,
+                        modified_by: queryData.modified_by,
+                        date_added: queryData.date_added,
+                        date_modified: queryData.date_modified
                     };
 
                     $scope.data.labels = [];
@@ -192,7 +215,11 @@
                 id: $scope.data.id,
                 status: $scope.data.status,
                 priority: $scope.data.priority,
-                title: $scope.data.title
+                title: $scope.data.title,
+                created_by: $scope.data.created_by,
+                modified_by: $scope.username,
+                date_added: $scope.data.date_added,
+                date_modified: $scope.FromDate
 
             });
 
@@ -214,6 +241,8 @@
 
             var root = firebase.database().ref();
             root.child('task/' + taskID + '/status').set('on the process');
+            root.child('task/' + taskID + '/date_modified').set($scope.FromDate);
+            root.child('task/' + taskID + '/modified_by').set($scope.username);
 
             $scope.firebase();
             $scope.initializeData();
@@ -224,6 +253,8 @@
 
             var root = firebase.database().ref();
             root.child('task/' + taskID + '/status').set('completed');
+            root.child('task/' + taskID + '/date_modified').set($scope.FromDate);
+            root.child('task/' + taskID + '/modified_by').set($scope.username);
 
             $scope.firebase();
             $scope.initializeData();
@@ -238,6 +269,156 @@
 
             $scope.firebase();
             $scope.initializeData();
+
+        }
+
+        $scope.authentication = function(){
+
+            var uiConfig = {
+                'callbacks': {
+                    // Called when the user has been successfully signed in.
+                    'signInSuccess': function(user, credential, redirectUrl) {
+                        //handleSignedInUser(user);
+                        // Do not redirect.
+                        return false;
+                    }
+                },
+                'signInFlow': 'popup',
+                'signInOptions': [
+                    // Leave the lines as is for the providers you want to offer your users.
+                    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+                    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+                    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+                    firebase.auth.EmailAuthProvider.PROVIDER_ID
+                ],
+                // Terms of service url.
+                'tosUrl': 'https://console.firebase.google.com/project/real-time-task-manager/overview',
+            };
+
+
+            firebase.auth().onAuthStateChanged(function (user) {
+                if (user) {
+                    // User is signed in.
+
+                    $scope.userHandler(user);
+
+                    var displayName = user.displayName;
+                    var email = user.email;
+                    var emailVerified = user.emailVerified;
+                    var photoURL = user.photoURL;
+                    var uid = user.uid;
+                    var providerData = user.providerData;
+
+                    var userDetails = {
+
+                        displayName: displayName,
+                        email: email,
+                        emailVerified: emailVerified,
+                        photoURL: photoURL,
+                        uid: uid,
+                        providerData: providerData
+
+                    }
+
+
+                    $('.login-container').css('display', 'none');
+                    $('.app-container').css('display', 'block');
+
+                    if(userDetails.displayName == null){
+
+
+                        $.each(userDetails.providerData, function(key, value){
+
+                            $.each(value, function(index, data){
+
+                                //console.log( index, data);
+                                if(index == 'displayName' ){
+                                    $('.username').html(data);
+                                }
+                                if(index == 'photoURL'){
+                                    $('.user-photo').attr('src', data);
+                                }
+
+                            })
+
+                        });
+
+                    }else{
+
+                        $('.username').html(userDetails.displayName);
+                        $('.user-photo').attr('src', userDetails.photoURL);
+                    }
+
+
+
+
+
+                } else {
+                    // User is signed out.
+
+                    console.log('User is signed out');
+
+                    // Initialize the FirebaseUI Widget using Firebase.
+                    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+                    // The start method will wait until the DOM is loaded.
+                    ui.start('#firebaseui-auth-container', uiConfig);
+
+                    $('.login-container').css('display', 'block');
+                    $('.app-container').css('display', 'none');
+
+                }
+            }, function (error) {
+                console.log(error);
+            });
+
+        }
+
+        $scope.userHandler = function(user){
+
+            $scope.username = '';
+
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var providerData = user.providerData;
+
+            var userDetails = {
+
+                displayName: displayName,
+                email: email,
+                emailVerified: emailVerified,
+                photoURL: photoURL,
+                uid: uid,
+                providerData: providerData
+
+            }
+
+
+            if(userDetails.displayName == null){
+
+                $.each(userDetails.providerData, function(key, value){
+
+                    $.each(value, function(index, data){
+
+                        if(index == 'displayName' ){
+                            $scope.username = data;
+                            $scope.$apply();
+                        }
+
+                    })
+
+                });
+
+            }else{
+
+                $scope.username = userDetails.displayName;
+                $scope.$apply();
+
+            }
+
 
         }
 
